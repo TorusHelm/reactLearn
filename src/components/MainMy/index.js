3e0fd44fda2b89bf7fd2cf7dea8005b3f65c71fe
TorusHelm <TorusHelm@list.ru>
2019-04-08 15:53:14 +0500

    process

--- src/components/MainMy/index.js
+++ src/components/MainMy/index.js
@@ -2,25 +2,31 @@ import React from 'react';
 import {Container} from 'react-bootstrap';
 
 function MainMy() {
+
+    const firstName = "Torus";
+    const lastName = "Helm";
+    const date = new Date();
+    const hours = date.getHours();
+    let timeOfDay;
+    const styles = {
+        fontSize : 30
+    }
+
+    if (hours < 12) {
+        timeOfDay = "morning"
+        styles.color = "lightblue"
+    } else if (hours >= 12 && hours < 17) {
+        timeOfDay = "afternoon"
+        styles.color = "tomato"
+    } else {
+        timeOfDay = "night"
+        styles.color = "green"
+    }
+    
     return (
         <main>
             <Container className="pt-3">
-                <div className="custom-control custom-checkbox alert alert-primary pl-5">
-                    <input type="checkbox" className="custom-control-input" id="customCheck-1" />
-                    <label className="custom-control-label" htmlFor="customCheck-1">First Variant</label>
-                </div>
-                <div className="custom-control custom-checkbox alert alert-primary pl-5">
-                    <input type="checkbox" className="custom-control-input" id="customCheck-2" />
-                    <label className="custom-control-label" htmlFor="customCheck-2">Second Variant</label>
-                </div>
-                <div className="custom-control custom-checkbox alert alert-primary pl-5">
-                    <input type="checkbox" className="custom-control-input" id="customCheck-3" />
-                    <label className="custom-control-label" htmlFor="customCheck-3">Third Variant</label>
-                </div>
-                <div className="custom-control custom-checkbox alert alert-primary pl-5">
-                    <input type="checkbox" className="custom-control-input" id="customCheck-4" />
-                    <label className="custom-control-label" htmlFor="customCheck-4">Fourth Variant</label>
-                </div>
+                <h1 style={styles}>Good {`${timeOfDay} ${firstName} ${lastName}`}!</h1>
             </Container>
         </main>
     )
