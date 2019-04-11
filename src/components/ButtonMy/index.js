70c533dc94ad0a65ac8d04a46b3cf71aa1b808b1
TorusHelm <TorusHelm@list.ru>
2019-04-11 12:29:38 +0500

    learn lifecicles didMount, willReciveProps, willUnmount, shuldUpdate

--- src/components/ButtonMy/index.js
+++ src/components/ButtonMy/index.js
@@ -6,6 +6,17 @@ class ButtonMy extends React.Component {
     componentWillReceiveProps(nextProps) {
         console.log(nextProps)
     }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        // вернуть true если компонент нуждается в обновлении
+        // вернуть false в противном случае
+        const $this = this.props
+        if(nextProps === $this) {
+            return false
+        } else {
+            return true
+        }
+    }
     
     render() {
         return (
